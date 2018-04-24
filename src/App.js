59b2db7bc379ef903f01f02119808030f330e473
oreoluwade <oreoluwade@gmail.com>
2018-04-24 10:19:20 +0100

    Configure router, show evidence of routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import { LandingPage, AboutPage } from './components';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/about" component={AboutPage}/>
+        </Switch>
+      </Router>
     );
   }
 }

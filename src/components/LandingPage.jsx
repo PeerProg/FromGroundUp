a41301c57f34de30778b41e0818e6e9b9ed43724
KniteDev <bmose@atbtechsoft.com>
2018-11-22 19:36:37 +0100

    Connect to Backend and Display Data from Backend

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    fetch('/home')
+      .then(res => res.json())
+      .then(user => setUser(user))
+  }, [user])
+
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="App-title">Welcome to The Habit Tracker Application</h1>
+        <h3>
+          {user.message}
+        </h3>
       </header>
       <div className="App-intro">
 

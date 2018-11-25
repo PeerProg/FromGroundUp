542d73216281d85f26d1be1ba787469f47219803
KniteDev <bmose@atbtechsoft.com>
2018-11-25 11:22:46 +0100

    Add register and login Component

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import {
   Switch,
   Route
 } from 'react-router-dom';
-import { LandingPage, AboutPage } from './components';
+import { LandingPage, AboutPage, RegisterPage, LoginPage } from './components';
 import './App.css';
 
 const App = () => {
 
     return (
       <div>
- 
       <Router>
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route exact path="/about" component={AboutPage}/>
+            <Route exact path="/register" component={RegisterPage} />
+            <Route exact path="/login" component={LoginPage} />
         </Switch>
       </Router>
       </div>

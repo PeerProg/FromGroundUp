e50bafc8d317b8211fd20dd871b5e13ef20b9778
KniteDev <bmose@atbtechsoft.com>
2018-11-25 12:40:08 +0100

    remove  the Api calls

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,19 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import requestHandler from '../services/requestHandler'
 
 
 const LandingPage = () => {
-  const [landingPageUrl, setlandingPageUrl] = useState([]);
-  
-  useEffect(() => {
-    requestHandler.homeUrl().then(res => setlandingPageUrl(res.data.message))
-  }, [landingPageUrl])
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">{landingPageUrl}</h1>
+        <h1 className="App-title">Welcome Home</h1>
       </header>
       <div className="App-intro">
 

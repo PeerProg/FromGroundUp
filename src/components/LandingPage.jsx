a1c9b831f8045ea8d946e8faba39789aefb41821
KniteDev <bmose@atbtechsoft.com>
2018-11-27 15:11:31 +0100

    display username when logged in on the landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import requestHandler from '../services/requestHandler'
+import { AppConsumer } from '../context';
+
 
 const LandingPage = () => {
   const [landingPageContent, setlandingPageContent] = useState('');
@@ -8,13 +10,21 @@ const LandingPage = () => {
     requestHandler.homeUrl().then(res => setlandingPageContent(res.data.message))
   }, [landingPageContent])
   return (
+    <AppConsumer>
+      {({ username }) => (
     <div className="App">
       <div className="App-intro">
         <h2>
           {landingPageContent}
         </h2>
+
+        {username ?
+          <h2 >Welcome {username}</h2> : ''
+        }
       </div>
     </div>
+      )}
+    </AppConsumer>
   )
 };
 

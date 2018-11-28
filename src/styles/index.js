7949c3e1fe3fb7ab277a6ebf9ab9c71e4a1e44d2
oreoluwade <oreoluwade@gmail.com>
2018-11-28 08:42:38 +0100

    abstract styles

---
+++ src/styles/index.js
@@ -0,0 +1,22 @@
+export const submitButtonStyle = {
+  width: '200px',
+  height: '40px',
+  backgroundColor: '#2BB2ED',
+  fontSize: '30px',
+  borderRadius: '2px'
+}
+
+export const authFieldStyle = {
+  width: '300px',
+  height: '40px',
+  fontSize: '25px',
+  backgroundColor: '#E3E4E5',
+  borderStyle: 'solid'
+}
+
+export const loginFormContainerStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+}

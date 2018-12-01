5d375caf8160997daab8d895a82563d6ca16fba3
KniteDev <bmose@atbtechsoft.com>
2018-12-01 14:39:09 +0100

    create dashboard component and route

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div className="App">
+      <h1>This is the habit tracker Dashboard</h1>
+    </div>
+  );
+};
+
+export default Dashboard;

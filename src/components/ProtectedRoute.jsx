418ffe676edff213940722e5462bb8927a44105c
oreoluwade <oreoluwade@gmail.com>
2018-12-01 21:54:11 +0100

    refine ProtectedRoute logic

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,15 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { UserConsumer } from '../contexts';
 
 function ProtectedRoute(props) {
-  return (
-    <UserConsumer>
-      {({ isAuthenticated }) =>
-        isAuthenticated ? <Route {...props} /> : <Redirect to="/login" />
-      }
-    </UserConsumer>
-  );
+  const [pageShouldLoad, setPageShouldLoad] = useState(null);
+  useEffect(async () => {
+    const tokenIsAvailable = await !!localStorage.getItem('jwtToken');
+    setPageShouldLoad(tokenIsAvailable);
+  }, []);
+  return pageShouldLoad ? <Route {...props} /> : <Redirect to="/login" />;
 }
 
 export default ProtectedRoute;

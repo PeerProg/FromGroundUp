57627ae4112082a8b9f71bad758e8579c67b95f5
KniteDev <bmose@atbtechsoft.com>
2018-12-02 22:21:08 +0100

    show my habits navigation on header once logged in

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -55,6 +55,13 @@ const Header = () => {
                   </Link>
                 </li>
                 <li className="nav-item">
+                  <Link to={`/habits/${user.id}`} className="nav-link">
+                    <button className="btn btn-outline-primary customBtn">
+                      My Habits
+                    </button>
+                  </Link>
+                </li>
+                <li className="nav-item">
                   <Link to="/" className="nav-link">
                     <button
                       className="btn btn-outline-primary customBtn"

71dba8b70534a3f93b36c1295744fe3926649363
KniteDev <bmose@atbtechsoft.com>
2018-12-02 17:30:58 +0100

    readd styles file

---
+++ src/styles/index.js
@@ -0,0 +1,62 @@
+export const submitButtonStyle = {
+  width: '200px',
+  height: '40px',
+  backgroundColor: '#2096F3',
+  fontSize: '17px',
+  boxShadow: '0 4px 8px 1px rgba(0, 0, 0, 0.3)',
+  textTransform: 'uppercase'
+};
+
+export const authFieldStyle = {
+  width: '300px',
+  height: '40px',
+  fontSize: '15px'
+};
+
+export const loginFormContainerStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center'
+};
+
+export const headerStyles = {
+  AppBar: {
+    backgroundColor: '#2096F3'
+  },
+  linkText: {
+    textDecoration: 'none',
+    color: 'white',
+    fontFamily: 'Roboto'
+  }
+};
+
+export const authDivContainerStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  height: '100%',
+  justifyContent: 'center',
+  marginRight: 'auto',
+  marginLeft: 'auto',
+  width: '40%',
+  marginTop: '5%'
+};
+
+export const authFormContainerStyle = {
+  width: '100%',
+  paddingLeft: '0',
+  paddingRight: '0',
+  textAlign: 'center',
+  marginTop: '3%'
+};
+
+export const profileCard = {
+  width: '40%',
+  height: '300px',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  display: 'flex',
+  marginTop: '5%',
+  justifyContent: 'center'
+};

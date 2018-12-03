f77e32d938469759d085899421399a65fd399dc3
oreoluwade <oreoluwade@gmail.com>
2018-12-03 15:25:29 +0100

    make provision for habit name render on habits page

--- src/components/HabitsPage.jsx
+++ src/components/HabitsPage.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useEffect, useContext, useState } from 'react';
+import { userContext } from '../contexts';
+import { showMyHabits } from '../services';
+
+const getUserHabits = async userId => {
+  try {
+    const response = await showMyHabits(userId);
+    return response;
+  } catch (error) {
+    return error;
+  }
+};
 
 function HabitsPage() {
+  const context = useContext(userContext);
+  const [habits, setHabits] = useState([]);
+
+  useEffect(
+    () => {
+      getUserHabits(context.user.id).then(result => {
+        setHabits(result.data);
+      });
+    },
+
+    [context.user.id]
+  );
+
   return (
-    <div>
-      <h1>This is the habits page</h1>
-    </div>
+    <React.Fragment>
+      <h1>Track your habits and milestones</h1>
+      {habits && habits.map(habit => <p key={habit.id}>{habit.name}</p>)}
+    </React.Fragment>
   );
 }
 
--- src/services/habitServices.js
+++ src/services/habitServices.js
@@ -1,10 +1,8 @@
 import Api from './Api';
 
 export const showMyHabits = userId => {
-  if (userId) {
-    return Api()
-      .get(`api/v1/habit/user/${userId}/all-habits`)
-      .then(res => Promise.resolve(res.data))
-      .catch(err => Promise.reject(err.response.data.error));
-  }
+  return Api()
+    .get(`api/v1/habit/user/${userId}/all-habits`)
+    .then(res => Promise.resolve(res.data))
+    .catch(err => Promise.reject(err.response.data.error));
 };

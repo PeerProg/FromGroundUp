7941c299fb4666a59259b7f3ecbd6ce51e4e36e9
KniteDev <bmose@atbtechsoft.com>
2018-12-13 11:18:03 +0100

    refactor profileview to accomodate imageURL input

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,81 +1,147 @@
 import React from 'react';
+import { useState } from 'react';
 import swal from 'sweetalert2';
 import { Formik, Form, Field } from 'formik';
 import { updateUserInfo } from '../services';
 import { CustomInput } from '.';
-import {
-  submitButtonStyle,
-  loginFormContainerStyle,
-  profileCard
-} from '../styles';
+import { submitButtonStyle, loginFormContainerStyle } from '../styles';
 import { UserConsumer } from '../contexts';
 import { profilePageValidator } from '../helpers';
+import imageUrl from '../images/imageURL.jpg';
+import noImageUrl from '../images/noImageUrl.png';
 
 const ProfilePage = props => {
+  const [editStatus, setEditStatus] = useState(false);
+  const [editButtonName, seteditButtonName] = useState('Edit Profile');
+
+  const handleOnEdit = () => {
+    if (editButtonName === 'Edit Profile') {
+      seteditButtonName('Finished');
+      setEditStatus(!editStatus);
+    } else {
+      seteditButtonName('Edit Profile');
+      setEditStatus(!editStatus);
+    }
+  };
+
   return (
-    <div className="card" style={profileCard}>
+    <div>
       <UserConsumer>
         {({ user, handleUserData }) => (
-          <Formik
-            initialValues={{ username: user.username, email: user.email }}
-            validate={values => profilePageValidator(values)}
-            onSubmit={async ({ username, email }, { setSubmitting }) => {
-              updateUserInfo({ username, email, id: props.match.params.userId })
-                .then(res => {
-                  handleUserData({ username, email });
-                  swal({
-                    type: 'success',
-                    position: 'top-end',
-                    title: 'Update Successful',
-                    toast: true,
-                    showConfirmButton: false,
-                    timer: 3000
-                  });
+          <div className="card profileCard">
+            <div className="profileImgBoxBackground">
+              <div className="editButton">
+                <button
+                  type="button"
+                  onClick={() => handleOnEdit()}
+                  className="btn btn-primary"
+                >
+                  {editButtonName}
+                </button>
+              </div>
+              <div className="profileImgBox">
+                {user.imageURL && (
+                  <img className="profileImg" src={imageUrl} alt="" />
+                )}
+                {!user.imageURL && (
+                  <img className="profileImg" src={noImageUrl} alt="" />
+                )}
+              </div>
+            </div>
+
+            <div className="profileDetails">
+              <h3>
+                Name: <span>{user.username}</span> <br />
+                Email: <span>{user.email}</span> <br />
+              </h3>
+            </div>
+
+            <Formik
+              initialValues={{
+                username: user.username,
+                email: user.email,
+                imageURL: user.imageURL
+              }}
+              validate={values => profilePageValidator(values)}
+              onSubmit={async (
+                { username, email, imageURL },
+                { setSubmitting }
+              ) => {
+                if (imageURL.length === 0) {
+                  imageURL = null;
+                }
+                updateUserInfo({
+                  username,
+                  email,
+                  imageURL,
+                  id: props.match.params.userId
                 })
-                .catch(err => {
-                  swal({
-                    type: 'error',
-                    position: 'top-end',
-                    title: err.message,
-                    toast: true,
-                    showConfirmButton: false,
-                    timer: 3000
+                  .then(res => {
+                    handleUserData({ username, email, imageURL });
+                    swal({
+                      type: 'success',
+                      position: 'top-end',
+                      title: 'Update Successful',
+                      toast: true,
+                      showConfirmButton: false,
+                      timer: 3000
+                    });
+                  })
+                  .catch(err => {
+                    swal({
+                      type: 'error',
+                      position: 'top-end',
+                      title: err.message,
+                      toast: true,
+                      showConfirmButton: false,
+                      timer: 3000
+                    });
                   });
-                });
-              setSubmitting(false);
-            }}
-          >
-            {({ isSubmitting }) => (
-              <Form>
-                <div style={loginFormContainerStyle}>
-                  <Field
-                    type="text"
-                    name="username"
-                    placeholder="Username"
-                    component={CustomInput}
-                  />
-                  <br />
-                  <br />
-                  <Field
-                    type="text"
-                    name="email"
-                    placeholder="Email"
-                    component={CustomInput}
-                  />
-                  <br />
-                  <br />
-                  <button
-                    className="btn btn-primary customBtn"
-                    type="submit"
-                    disabled={isSubmitting}
-                    style={submitButtonStyle}
-                  >
-                    Submit
-                  </button>
-                </div>
-              </Form>
-            )}
-          </Formik>
+                setSubmitting(false);
+              }}
+            >
+              {({ isSubmitting }) => (
+                <Form className="formStyle">
+                  {editStatus && (
+                    <div style={loginFormContainerStyle}>
+                      <Field
+                        type="text"
+                        name="username"
+                        placeholder="Username"
+                        component={CustomInput}
+                      />
+                      <br />
+                      <br />
+                      <Field
+                        type="text"
+                        name="email"
+                        placeholder="Email"
+                        component={CustomInput}
+                      />
+                      <br />
+                      <br />
+                      <Field
+                        type="text"
+                        name="imageURL"
+                        placeholder="imageURL"
+                        component={CustomInput}
+                      />
+                      <br />
+                      <br />
+                      <button
+                        className="btn btn-primary customBtn"
+                        type="submit"
+                        disabled={isSubmitting}
+                        style={submitButtonStyle}
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  )}
+                </Form>
+              )}
+            </Formik>
+          </div>
         )}
       </UserConsumer>
     </div>

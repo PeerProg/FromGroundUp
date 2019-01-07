8d66c4a719a40e6c47b51cce0b043ffb226d44df
KniteDev <bmose@atbtechsoft.com>
2019-01-07 17:54:56 +0100

    refactor habitPage and populate with localstorage info

--- src/components/HabitsPage.jsx
+++ src/components/HabitsPage.jsx
@@ -2,8 +2,13 @@ import React, { useEffect, useContext, useReducer } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { userContext } from '../contexts';
 import { fetchMyHabits } from '../services';
-import FancyDiv from './FancyDiv';
-import Milestones from './Milestones';
+
+const initialVaues = {
+  name: '',
+  days: '',
+  todayDate: '',
+  expiresDate: ''
+};
 
 const reducer = (previousState, newState) => {
   return { ...previousState, ...newState };
@@ -20,10 +25,14 @@ const getUserHabits = async userId => {
 
 const HabitsPage = () => {
   const context = useContext(userContext);
-  const [{ indexOfHabitClicked, habits }, setState] = useReducer(reducer, {
-    indexOfHabitClicked: -1,
-    habits: []
-  });
+  const [{ indexOfHabitClicked, habits, habitInfo }, setState] = useReducer(
+    reducer,
+    {
+      indexOfHabitClicked: -1,
+      habits: [],
+      habitInfo: JSON.parse(localStorage.getItem('habitInfo')) || initialVaues
+    }
+  );
 
   useEffect(
     () => {
@@ -48,47 +57,70 @@ const HabitsPage = () => {
         <h2 className="text-monospace">Track your habits and milestones</h2>
       </div>
       <React.Fragment>
-        {habits &&
-          habits.map((habit, index) => (
-            <div key={habit.name}>
-              <FancyDiv
-                className="d-flex bd-highlight mb-2"
-                color={index % 2 === 0 ? '#F7F7F7' : 'white'}
-                onClick={() => showSubContent(index)}
-              >
-                <span className="mr-auto p-2 bd-highlight font-weight-bold text-monospace">
-                  {habit.name}
-                </span>
-                <span className="p-2 bd-highlight">
-                  <FontAwesomeIcon
-                    icon="plus-circle"
-                    className="mr-4 fa-lg"
-                    color="#76B439"
-                    data-toggle="tooltip"
-                    title="Show Milestones"
-                    style={{ cursor: 'pointer' }}
-                  />
-                  <FontAwesomeIcon
-                    icon="edit"
-                    className="mr-4 fa-lg"
-                    color="#76B439"
-                    data-toggle="tooltip"
-                    title="Edit Habit"
-                    style={{ cursor: 'pointer' }}
-                  />
-                  <FontAwesomeIcon
-                    icon="trash-alt"
-                    color="#8F1012"
-                    className="fa-lg"
-                    data-toggle="tooltip"
-                    title="Delete Habit"
-                    style={{ cursor: 'pointer' }}
-                  />
-                </span>
-              </FancyDiv>
-              {index === indexOfHabitClicked && <Milestones />}
-            </div>
-          ))}
+        <div className="table-responsive">
+          <table className="table table-d table-striped table-bordered">
+            <thead className="thead-dark">
+              <tr>
+                <th scope="col">Properties</th>
+                <th scope="col">#</th>
+                <th scope="col">Habit Title</th>
+                <th scope="col">Date Created</th>
+                <th scope="col">Expected Date of Completion</th>
+                <th scope="col">Number of Days Remaining</th>
+              </tr>
+            </thead>
+            <tbody>
+              {habits &&
+                habits.map((habit, index) => (
+                  <tr
+                    key={habit.name}
+                    data-toggle="modal"
+                    data-target="#exampleModal"
+                    onClick={() => showSubContent(index)}
+                  >
+                    <td>
+                      <FontAwesomeIcon
+                        icon="plus-circle"
+                        className="mr-4 fa-lg"
+                        color="#76B439"
+                        data-toggle="tooltip"
+                        title="Show Milestones"
+                        style={{ cursor: 'pointer' }}
+                      />
+                      <FontAwesomeIcon
+                        icon="edit"
+                        className="mr-4 fa-lg"
+                        color="#76B439"
+                        data-toggle="tooltip"
+                        title="Edit Habit"
+                        style={{ cursor: 'pointer' }}
+                      />
+                      <FontAwesomeIcon
+                        icon="trash-alt"
+                        color="#8F1012"
+                        className="fa-lg"
+                        data-toggle="tooltip"
+                        title="Delete Habit"
+                        style={{ cursor: 'pointer' }}
+                      />
+                    </td>
+                    <th scope="row">{index + 1}</th>
+                    <td> {habit.name}</td>
+                    {habit.name.toLowerCase() ===
+                      habitInfo.name.toLowerCase() && (
+                      <td>{habitInfo.todayDate}</td>
+                    )}
+                    {habit.name.toLowerCase() ===
+                      habitInfo.name.toLowerCase() && (
+                      <td>{habitInfo.expiresDate}</td>
+                    )}
+                    {habit.name.toLowerCase() ===
+                      habitInfo.name.toLowerCase() && <td>{habitInfo.days}</td>}
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
       </React.Fragment>
     </div>
   );

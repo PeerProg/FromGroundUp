eed94338c756a648a59e76032b295473bc03fab9
KniteDev <bmose@atbtechsoft.com>
2019-01-30 21:18:05 +0100

    create default habitState for context and localstorage consumption

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,3 @@
 export { default as setAuthorizationToken } from './setAuthorizationToken';
 export { initialUserState } from './initalUserState';
+export { initialHabitState } from './initalHabitState';

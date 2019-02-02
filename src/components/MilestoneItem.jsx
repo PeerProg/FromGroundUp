bc2a11df35bb9ca9cffdc64fbdd4e3495266ab82
oreoluwade <oreoluwade@gmail.com>
2019-02-02 11:09:18 +0100

    refactor rendered dates display

--- src/components/MilestoneItem.jsx
+++ src/components/MilestoneItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import * as moment from 'moment';
+import { standardizeDate } from '../helpers';
 
 const MilestoneItem = ({ title, date }) => {
   return (
@@ -15,7 +15,7 @@ const MilestoneItem = ({ title, date }) => {
       />
       <span className="text-monospace ml-5">{title}</span>
       <span className="text-monospace ml-auto mr-5">
-        {moment(new Date(date)).format('LLL')}
+        {standardizeDate(date)}
       </span>
     </div>
   );

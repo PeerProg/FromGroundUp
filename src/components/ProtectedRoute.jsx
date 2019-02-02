4e803b14b595f808fe2b37799ca255a683f003bf
oreoluwade <oreoluwade@gmail.com>
2019-02-02 17:54:19 +0100

    refactor ProtectedRoutes logic

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,34 +1,32 @@
 import React, { useContext } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { userContext } from '../contexts';
-import { setAuthorizationToken } from '../utils';
-import { validateToken } from '../services';
+import { setAuthorizationToken, tokenIsValid } from '../utils';
 import swal from 'sweetalert2';
 
 function ProtectedRoute(props) {
   const {
-    user: { token, id: userId },
+    user: { token },
     handleAuthStatus,
     isAuthenticated
   } = useContext(userContext);
+
   setAuthorizationToken(token);
 
-  // create an API caller to validate token with backEnd
-  validateToken(userId).catch(err => {
+  if (!tokenIsValid(token)) {
     swal({
       type: 'error',
       position: 'top-end',
-      title: err.message,
+      title: 'Token expired, login to continue',
       toast: true,
       showConfirmButton: false,
       timer: 3000
     });
-
     handleAuthStatus(false);
     localStorage.removeItem('jwtToken');
     localStorage.removeItem('userDetails');
-    setAuthorizationToken();
-  });
+  }
+
   return isAuthenticated ? <Route {...props} /> : <Redirect to="/login" />;
 }
 

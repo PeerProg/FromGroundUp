2c58e6e92ba8386d389864b379daf085ae33f1de
oreoluwade <oreoluwade@gmail.com>
2019-02-03 10:52:41 +0100

    add delete habit service

--- src/services/index.js
+++ src/services/index.js
@@ -3,5 +3,5 @@ export {
   loginUser,
   updateUserInfo,
 } from './userServices';
-export { fetchMyHabits, createNewHabit } from './habitServices';
+export { fetchMyHabits, createNewHabit, deleteHabit } from './habitServices';
 export { createNewMilestone, fetchMilestones } from './milestoneServices';

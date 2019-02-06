0f4612a663d9ec444113d51f890fbe1a2af178b2
oreoluwade <oreoluwade@gmail.com>
2019-02-06 07:53:20 +0100

    update getDurationToExpiration function

--- src/helpers/getDurationToExpiration.js
+++ src/helpers/getDurationToExpiration.js
@@ -2,6 +2,7 @@ export default expiryDate => {
   const endDate = new Date(expiryDate);
   const today = new Date();
   const ONE_HOUR_IN_MS = 1000 * 60 * 60;
+  if (today > endDate) return 'Expired';
   const RAW_TOTAL_DURATION_IN_HOURS = (endDate - today) / ONE_HOUR_IN_MS;
   const ROUNDED_DURATION_IN_HOURS = Math.round(RAW_TOTAL_DURATION_IN_HOURS);
   const ROUNDED_DURATION_IN_DAYS = Math.round(RAW_TOTAL_DURATION_IN_HOURS / 24);
